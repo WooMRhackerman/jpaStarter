4a919ca07bf8440292efc60bba5f86eaaebad345
DESKTOP-KNDQ50R\admin <sad2222@naver.com>
2022-07-16 21:43:58 +0900

    스프링 실전
    엔티티 생성

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter 
+public class Address {
+	private String city;
+	private String street;
+	private String zipcode;
+	
+	protected Address(String city, String street, String zipcode) {
+		this.city = city;
+		this.street = street;
+		this.zipcode = zipcode;
+	}
+	
+}
